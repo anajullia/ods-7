add450ff12cf3860809203d9c079e7d10996cf73
meunome <meunome@meunome>
2022-11-20 14:30:15 -0300

    Ajuste XML e importando fonte

--- js/conteudo.js
+++ js/conteudo.js
@@ -12,13 +12,6 @@ x = xmlDoc.getElementsByTagName("corpo");
 function texto1(){
     for(i=0;i<=1;i++){
         document.write(
-           x[i].getElementsByTagName("texto1")[0].childNodes[0].nodeValue);
-    }
-}
-
-function imagem1(){
-    for(i=0;i<=1;i++){
-        document.write(
-           x[i].getElementsByTagName("imagem1")[0].childNodes[0].nodeValue);
+           "<p class='textoajuste1'>" + x[i].getElementsByTagName("texto1")[0].childNodes[0].nodeValue + "</p>");
     }
 }
